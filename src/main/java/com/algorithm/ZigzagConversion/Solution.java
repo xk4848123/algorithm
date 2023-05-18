df62c95ee9f6f6fb1c050d146d3a6146254413c3
chendi <15395068264@163.com>
2023-05-18 21:09:25 +0800

    feat:增加ZigzagConversion算法

---
+++ src/main/java/com/algorithm/ZigzagConversion/Solution.java
@@ -0,0 +1,16 @@
+package com.algorithm.ZigzagConversion;
+
+public class Solution {
+
+    public String convert(String s, int numRows) {
+
+        char[][] charsArray = new char[numRows][s.length()];
+
+        for (int i = 0, j = 0, k = 0; k < s.length(); k++) {
+
+        }
+
+
+    }
+
+}
