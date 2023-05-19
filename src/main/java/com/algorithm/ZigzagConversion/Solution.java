9d1f4dcf88933460d7eb880cae73bb36b3ed1077
chendi <15395068264@163.com>
2023-05-19 15:53:24 +0800

    feat:增加reverse和ZigzagConversion

--- src/main/java/com/algorithm/ZigzagConversion/Solution.java
+++ src/main/java/com/algorithm/ZigzagConversion/Solution.java
@@ -1,16 +1,47 @@
 package com.algorithm.ZigzagConversion;
 
-public class Solution {
-
+class Solution {
     public String convert(String s, int numRows) {
 
-        char[][] charsArray = new char[numRows][s.length()];
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
 
+        Character[][] charsArray = new Character[numRows][s.length()];
+
+        //flag为false时代表从上往下，true代表从左下往右上
+        boolean flag = false;
+
+        //摆好位置
         for (int i = 0, j = 0, k = 0; k < s.length(); k++) {
+            charsArray[i][j] = s.charAt(k);
+
+            if (!flag) {
+                if (++i == numRows - 1) {
+                    flag = true;
+                }
+            } else {
+                j++;
+                if (--i == 0) {
+                    flag = false;
+                }
+            }
+
 
         }
 
+        //按行读取
+        StringBuffer res = new StringBuffer();
 
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < s.length(); j++) {
+                Character c = charsArray[i][j];
+                if (c != null) {
+                    res.append(c);
+                }
+
+            }
+        }
+        return res.toString();
     }
-
 }
